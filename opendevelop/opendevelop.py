98a7bf89a1d5ba02e5623ebb65ec0735cb0aef60
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-12-18 14:20:46 +0000

    Implemented request and authenticated_request
    Implemented:
      * OpenDevelop._auth_header
      * OpenDevelop.request
      * OpenDevelop.authenticated_request
      * OpenDevelop.images

--- opendevelop/opendevelop.py
+++ opendevelop/opendevelop.py
@@ -1,3 +1,4 @@
+import base64
 import os
 import requests
 
@@ -13,3 +14,31 @@ class OpenDevelop(object):
             self.client_id = client_id
         if (client_secret):
             self.client_secret = client_secret
+
+    @property
+    def _auth_header(self):
+        auth_tuple = (client_id, client_secret)
+        auth_bearer = base64.standard_b64encode('%s:%s' % auth_tuple)
+        header_value = 'Basic %s' % auth_bearer
+        return header_value
+
+    def request(self, method, resource, payload='', headers={}):
+        url = 'http://%s/api/%s' % (self.host, resource)
+        response = requests.request(method, url, data=payload, headers=headers)
+        return response.json()
+
+    def authenticated_request(self, method, resource, payload='', headers={}):
+        headers['Authorization'] = self._auth_header
+        response = self.request(method, resource, payload, headers)
+
+    def images(self):
+        return self.request('get', 'images')
+
+    def create_sandbox(self):
+        pass
+
+    def sandbox(self):
+        pass
+
+    def sandbox_logs(self):
+        pass
