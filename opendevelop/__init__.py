abd7ad2d966167da880564ec4b1c36b9b119f845
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-12-20 12:07:55 +0000

    Documented the code

--- opendevelop/__init__.py
+++ opendevelop/__init__.py
@@ -1,1 +1,8 @@
+"""
+The opendevelop package is used to keep definition of classes in the proper,
+semantically, places. Importing all the needed classes here makes it
+convenient for developers to use the package is if it was a single-file
+module.
+"""
+
 from .opendevelop import OpenDevelop
--- opendevelop/opendevelop.py
+++ opendevelop/opendevelop.py
@@ -1,14 +1,34 @@
+"""
+This module holds OpenDevelop, the main class of the package, which acts
+as the client to the OpenDevelop service.
+"""
+
 import base64
 import json
 import os
 import requests
 
+
 class OpenDevelop(object):
+    """
+    The main class of the opendevelop package. It is used to
+      1. view the available images
+      2. create a sandbox
+      3. inspect a sandbox' status
+    """
     host = os.getenv('OPENDEVELOP_HOST')
     client_id = os.getenv('OPENDEVELOP_CLIENT_ID')
     client_secret = os.getenv('OPENDEVELOP_CLIENT_SECRET')
 
     def __init__(self, host=None, client_id=None, client_secret=None):
+        """
+        Initializes the OpenDevelop client based to the given data. If any of
+        the arguments is noe being given, it will take its value from the
+        corresponding enviroment variable respectively:
+          * OPENDEVELOP_HOST
+          * OPENDEVELOP_CLIENT_ID
+          * OPENDEVELOP_CLIENT_SECRET
+        """
         if (host):
             self.host = host
         if (client_id):
@@ -18,12 +38,21 @@ class OpenDevelop(object):
 
     @property
     def _auth_header(self):
+        """
+        Returns the Authorization header that has to be set, based on the given
+        data.
+        """
         auth_tuple = (self.client_id, self.client_secret)
         auth_bearer = base64.standard_b64encode('%s:%s' % auth_tuple)
         header_value = 'Basic %s' % auth_bearer
         return header_value
 
     def request(self, method, resource, payload='', headers={}, files={}):
+        """
+        Performs an HTTP request to the OpenDevelop server connected. If the
+        HTTP response returns a status code >= 400, then an Exception is being
+        raised.
+        """
         url = 'http://%s/api/%s' % (self.host, resource)
         response = requests.request(method, url, data=payload, headers=headers,
                                     files=files)
@@ -36,14 +65,27 @@ class OpenDevelop(object):
 
     def authenticated_request(self, method, resource, payload='', headers={},
                               files={}):
+        """
+        Creates the proper OAuth2 header and delegates the request to
+        OpenDevelop.request, with the Authorization header set.
+        """
         headers['Authorization'] = self._auth_header
         response = self.request(method, resource, payload, headers, files)
         return response
 
     def images(self):
+        """
+        Return a list of available images for the OpenDevelop instance that the
+        client is connected to.
+        """
         return self.request('get', 'images')
 
     def create_sandbox(self, image, cmd, files=[]):
+        """
+        Create a sandbox and execute the given commands inside the sandbox.
+        If any files given, they should be uploaded to the sandbox' data
+        directory.
+        """
         if (type(cmd) == list):
             cmd = json.dumps(cmd)
         payload = {
@@ -62,5 +104,8 @@ class OpenDevelop(object):
                                           files=file_dict)
 
     def sandbox(self, sandbox_slug):
+        """
+        Return information about a sandbox in a Python dict
+        """
         resource = 'sandbox/%s' % sandbox_slug
         return self.authenticated_request('get', resource)
